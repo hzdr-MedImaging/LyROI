45f0c57bf65ce47cc34fc714e92dea7ac90cb6f1
Pavel Nikulin <p.nikulin@hzdr.de>
2025-12-16 16:17:13 +0100

    Nicer error print

--- lyroi/__init__.py
+++ lyroi/__init__.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from importlib.metadata import version, PackageNotFoundError, metadata
 import re
+import sys
+import signal
 
 __package__ = "lyroi"
 creation_date = datetime(2025, 11, 26)
@@ -17,3 +19,16 @@ now_date = datetime.now()
 date_str = now_date.strftime("%Y") if creation_date.year == now_date.year else creation_date.strftime("%Y") + "-" + now_date.strftime("%Y")
 __copyright__ = "Copyright (c) " + date_str + " " + author_str + ", www.hzdr.de"
 __license__ = meta.get("License-Expression")
+
+def error_handler(exctype, value, traceback):
+  print()
+  print("Error:", value, file=sys.stderr)
+  sys.exit(1)
+
+def exit_handler(signal, frame):
+  print()
+  print("User abort (CTRL-C) received.")
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, exit_handler)
+sys.excepthook = error_handler
